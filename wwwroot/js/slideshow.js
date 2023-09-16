486367596c3f8e449799816780cde2412b00ea86
lcminhbu <lminhbuu@gmail.com>
2023-09-16 21:28:39 +0700

    update index

--- wwwroot/js/slideshow.js
+++ wwwroot/js/slideshow.js
@@ -1,6 +1,5 @@
 function init_slide_show(image_urls) {
     banner = document.getElementById("slideshow-container");
-    console.log(banner);
     for (var i = 0; i < image_urls.length; i++) {
         let slide = document.createElement("div");
         slide.classList.add("slide");
@@ -15,16 +14,13 @@ function init_slide_show(image_urls) {
 let slide_index = 0;
 
 function slide_show() {
-    console.log(slide_index);
     let slides = document.getElementsByClassName("slide");
     for (let i = 0; i < slides.length; i++) {
         if (i === slide_index) {
             slides[i].style.opacity = 1;
-            console.log("1 "+i);
         }
         else {
             slides[i].style.opacity = 0;
-            console.log("2 "+i);
         }
     }
     slide_index = (slide_index + 1) % slides.length;
